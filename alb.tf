3f52b9e3de82ddd92dc8315a24bec84b5a34cc27
montibrunomiguel <montibrunomiguel@gmail.com>
2020-10-20 11:20:12 -0300

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,1 +1,38 @@
+#Master & Seach Head x NLB to Transit Account
+resource "aws_lb" "splunk_alb"{
+    for_each = toset(var.splunk_resource)
+    name = "splunk-${each.value}-alb"
+    internal = true
+    load_balancer_type = "application"
+    security_groups = [aws_security_group.splunk_sg.id, aws_security_group.splunk_alb_sg.id]
+  
+    subnet = [
+      var.private_subnet_A_id[var.environment],
+      var.private_subnet_C_id[var.environment]
+    ]
+    
+    enable_deletion_protection = true
+  
+    tags = {
+      Name = "Splunk_${each.value}_Lb"
+    }
+  }
 
+#Deployer
+resource "aws_lb" "splunk_deployer_alb"{
+    name = "splunk-deployer-alb"
+    internal = true
+    load_balancer_type = "application"
+    security_groups = [aws_security_group.splunk_sg.id, aws_security_group.splunk_alb_sg.id]
+  
+    subnet = [
+      var.private_subnet_A_id[var.environment],
+      var.private_subnet_C_id[var.environment]
+    ]
+    
+    enable_deletion_protection = true
+  
+    tags = {
+      Name = "Splunk_Deployer_Lb"
+    }
+  }
